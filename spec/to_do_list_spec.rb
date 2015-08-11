5ed0041813def43bb4f5bd3f4e3cd6214c34690a
Carlo De Gregorio & Kendra Ash <student@epicodus.com>
2015-08-11 10:10:22 -0700

    it takes a description on a task(1 passing test

--- spec/to_do_list_spec.rb
+++ spec/to_do_list_spec.rb
@@ -1,2 +1,11 @@
 require('rspec')
 require('to_do_list')
+
+describe(Task) do
+  describe('#description') do
+    it('lets you give the task a description') do
+      test_task = Task.new('shower the monkey')
+      expect(test_task.description()).to(eq('shower the monkey'))
+    end
+  end
+end
